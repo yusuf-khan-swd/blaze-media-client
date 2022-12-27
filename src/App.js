c68e267cbaec81fb3bd3a0bac7e948e3fb2b0c11
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-27 16:04:45 +0600

    light theme and dark theme implement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>
+    <div data-theme={`${darkTheme ? "dark" : "light"}`}>
       <h2>Hello World</h2>
+      <button onClick={() => setDarkTheme(!darkTheme)}>{darkTheme ? "Light" : "Dark"}</button>
     </div>
   );
 }
