c0b206f31b6ddcb3025e3c47bc620f3794fa219d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-28 22:53:38 +0600

    added google login. Register and login with email and password functionality added in auth provider.

--- src/Context/AuthProvider/AuthProvider.js
+++ src/Context/AuthProvider/AuthProvider.js
@@ -1,11 +1,44 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import app from '../../firebase/firebase.config';
+
+const auth = getAuth(app);
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [authLoading, setAuthLoading] = useState(false);
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setAuthLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const login = (email, password) => {
+    setAuthLoading(true);
+    return signInWithEmailAndPassword(auth, email, password)
+  };
+
+  const googleLogin = () => {
+    setAuthLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        setAuthLoading(false);
+      }
+    })
+
+    return () => unSubscribe();
+  }, []);
 
 
-  const authInfo = {};
+  const authInfo = { user, authLoading, googleLogin, createUser, login };
 
   return (
     <AuthContext.Provider value={authInfo}>
