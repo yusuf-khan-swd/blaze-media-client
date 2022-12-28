de090dde41a8d7b85320671483b9bf6ea0047057
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-29 00:52:02 +0600

    added container for navbar

--- src/Page/Shared/NavBar/NavBar.js
+++ src/Page/Shared/NavBar/NavBar.js
@@ -53,60 +53,62 @@ const NavBar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="container mx-auto">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {menuItems}
-          </ul>
+              {menuItems}
+            </ul>
+          </div>
+          <Link to={`/`} className="btn btn-ghost normal-case text-xl">
+            Blaze Media
+          </Link>
         </div>
-        <Link to={`/`} className="btn btn-ghost normal-case text-xl">
-          Blaze Media
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{menuItems}</ul>
-      </div>
-      <div className="navbar-end">
-        {
-          user?.uid ?
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar" title={` ${user.email}`}>
-                <div className="w-10 rounded-full">
-                  <FaUser className="w-full h-full"></FaUser>
-                </div>
-              </label>
-              <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li>
-                  <button onClick={handleLogOut}>
-                    Logout
-                  </button>
-                </li>
-                <li><a href="/">Settings</a></li>
-              </ul>
-            </div>
-            :
-            <Link to={`/login`} className="btn">Login</Link>
-        }
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{menuItems}</ul>
+        </div>
+        <div className="navbar-end">
+          {
+            user?.uid ?
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar" title={` ${user.email}`}>
+                  <div className="w-9 rounded-full p-1">
+                    <FaUser className="h-full w-full"></FaUser>
+                  </div>
+                </label>
+                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                  <li>
+                    <button onClick={handleLogOut}>
+                      Logout
+                    </button>
+                  </li>
+                  <li><a href="/">Settings</a></li>
+                </ul>
+              </div>
+              :
+              <Link to={`/login`} className="btn">Login</Link>
+          }
+        </div>
       </div>
     </div>
   );
