026d7d0cc645bf9682509ac8093609e4f554d274
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-28 18:37:51 +0600

    added margin for card

--- src/Page/LoginRegister/Register/Register.js
+++ src/Page/LoginRegister/Register/Register.js
@@ -25,96 +25,98 @@ const Register = () => {
   };
 
   return (
-    <div className="container mx-auto my-16">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100 mx-auto">
-          <div className="card-body">
-            <h2 className="text-xl text-center font-bold">Register Form</h2>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text text-md">Your Name</span>
-              </label>
-              <input
-                {...register("name", { required: "name field is required" })}
-                type="text"
-                placeholder="name"
-                className="input input-bordered"
-              />
-              <p className="text-red-600"><small>{errors.name?.message}</small></p>
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                {...register("email", { required: "email field is required" })}
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-              />
-              <p className="text-red-600"><small>{errors.email?.message}</small></p>
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Your Image</span>
-              </label>
-              <input
-                {...register("email", { required: "email field is required" })}
-                type="file"
-                placeholder="email"
-                className="file-input file-input-bordered w-full"
-              />
-              <p className="text-red-600"><small>{errors.email?.message}</small></p>
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+    <div className="container mx-auto mt-8 mb-16">
+      <div className="m-2">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100 mx-auto">
+            <div className="card-body">
+              <h2 className="text-xl text-center font-bold">Register Form</h2>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Password</span>
+                  <span className="label-text text-md">Your Name</span>
                 </label>
                 <input
-                  {...register("password", {
-                    required: "password field is required",
-                  })}
-                  type={`${showPassword ? "text" : "password"}`}
-                  placeholder="password"
+                  {...register("name", { required: "name field is required" })}
+                  type="text"
+                  placeholder="Name"
                   className="input input-bordered"
                 />
-                <p className="text-red-600"><small>{errors.password?.message}</small></p>
+                <p className="text-red-600"><small>{errors.name?.message}</small></p>
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Confirm Password</span>
+                  <span className="label-text">Email</span>
                 </label>
                 <input
-                  {...register("confirm", {
-                    required: "confirm password field is required",
-                  })}
-                  type={`${showPassword ? "text" : "password"}`}
-                  placeholder="password"
+                  {...register("email", { required: "email field is required" })}
+                  type="email"
+                  placeholder="Email"
                   className="input input-bordered"
                 />
-                <p className="text-red-600"><small>{errors.confirm?.message}</small></p>
+                <p className="text-red-600"><small>{errors.email?.message}</small></p>
               </div>
-            </div>
-            <div className="form-control">
-              <label className="label cursor-pointer justify-start">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your Image</span>
+                </label>
                 <input
-                  onClick={handleShowPassword}
-                  type="checkbox"
-                  className="checkbox checkbox-primary checkbox-sm mr-2"
+                  {...register("email", { required: "email field is required" })}
+                  type="file"
+                  placeholder="email"
+                  className="file-input file-input-bordered w-full"
                 />
-                <span className="label-text">Show Password</span>
-              </label>
-            </div>
-            <div className="form-control mt-3">
-              <button className="btn btn-primary">Register</button>
-            </div>
-            <div className="form-control">
-              <p>Already have an account? <Link className="text-blue-600" to={`/login`}>Please Login</Link></p>
+                <p className="text-red-600"><small>{errors.email?.message}</small></p>
+              </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    {...register("password", {
+                      required: "password field is required",
+                    })}
+                    type={`${showPassword ? "text" : "password"}`}
+                    placeholder="Password"
+                    className="input input-bordered"
+                  />
+                  <p className="text-red-600"><small>{errors.password?.message}</small></p>
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Confirm Password</span>
+                  </label>
+                  <input
+                    {...register("confirm", {
+                      required: "confirm password field is required",
+                    })}
+                    type={`${showPassword ? "text" : "password"}`}
+                    placeholder="Password"
+                    className="input input-bordered"
+                  />
+                  <p className="text-red-600"><small>{errors.confirm?.message}</small></p>
+                </div>
+              </div>
+              <div className="form-control">
+                <label className="label cursor-pointer justify-start">
+                  <input
+                    onClick={handleShowPassword}
+                    type="checkbox"
+                    className="checkbox checkbox-primary checkbox-sm mr-2"
+                  />
+                  <span className="label-text">Show Password</span>
+                </label>
+              </div>
+              <div className="form-control mt-3">
+                <button className="btn btn-primary">Register</button>
+              </div>
+              <div className="form-control">
+                <p>Already have an account? <Link className="text-blue-600" to={`/login`}>Please Login</Link></p>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
