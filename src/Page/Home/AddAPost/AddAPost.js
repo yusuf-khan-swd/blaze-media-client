f024cd04f7ecbd481cd68cdb568034ab5eb94da3
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-29 16:45:13 +0600

    image hosted to image bb successful

--- src/Page/Home/AddAPost/AddAPost.js
+++ src/Page/Home/AddAPost/AddAPost.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import "./AddAPost.css";
 
 const AddAPost = () => {
+  const [isDataLoading, setIsDataLoading] = useState(false);
 
   const handlePost = (event) => {
     event.preventDefault();
+    setIsDataLoading(true);
 
     const fileField = document.querySelector('input[type="file"]');
 
@@ -13,17 +15,42 @@ const AddAPost = () => {
     const str = postBody.replace(/\s\s+/g, " ");
 
     if (str.length < 1) {
+      setIsDataLoading(false);
       return toast.error("Please type what you are thinking");
     }
 
     handleImageUpload(fileField);
-    document.getElementById("postBody").innerHTML = "";
-    document.getElementById("postImage").value = "";
+    // document.getElementById("postBody").innerHTML = "";
+    // document.getElementById("postImage").value = "";
   };
 
   const handleImageUpload = fileField => {
+    setIsDataLoading(true);
     const formData = new FormData();
-    console.log(fileField);
+    formData.append('image', fileField.files[0]);
+    console.log(formData);
+
+    fetch(`https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_ImageHostingKey}`, {
+      method: "POST",
+      body: formData
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.status === 200) {
+          toast.success("Image uploaded");
+          console.log(data);
+          setIsDataLoading(false);
+        }
+        else {
+          console.log("Image hosting problem", data);
+          toast.error(data.error.message);
+          setIsDataLoading(false);
+        }
+      })
+      .catch(error => {
+        console.log("Image hosting error", error);
+        setIsDataLoading(false);
+      })
   };
 
   return (
@@ -48,12 +75,14 @@ const AddAPost = () => {
                   accept="image/*"
                   className="file-input file-input-bordered w-full"
                   required
+                  disabled={isDataLoading}
                 />
               </div>
               <div className="form-control mt-3">
                 <button
                   type="submit"
                   className=" btn btn-primary capitalize text-xl h-10"
+                  disabled={isDataLoading}
                 >
                   Post
                 </button>
