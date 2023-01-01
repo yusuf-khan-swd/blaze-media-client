208b6b91a7d41fe6991728c2ad13297ba912a052
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-01 12:25:10 +0600

    added react query to project

--- src/Page/Media/Media/Media.js
+++ src/Page/Media/Media/Media.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Media = () => {
+
   return (
     <div>
       <h2>media page all posts and images will be shown one by one regarding the uploading serial</h2>
--- src/index.js
+++ src/index.js
@@ -5,13 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthProvider from './Context/AuthProvider/AuthProvider';
 import ThemeProvider from './Context/ThemeProvider/ThemeProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <ThemeProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ThemeProvider>
     </AuthProvider>
   </React.StrictMode>
