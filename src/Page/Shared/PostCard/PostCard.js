012730fa017523b8819e82cb189877471e07714a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-07 11:44:45 +0600

    handle likes count from client side

--- src/Page/Shared/PostCard/PostCard.js
+++ src/Page/Shared/PostCard/PostCard.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaRegCommentAlt, FaRegThumbsUp, FaThumbsUp } from 'react-icons/fa';
 
 const PostCard = ({ post }) => {
-  console.log(post);
+  const { postImgUrl, postBody, likes } = post;
 
-  const { postImgUrl, postBody } = post;
+  const [liked, setLiked] = useState(false);
+  const [totalLikes, setTotalLikes] = useState(likes)
+
+
+  const handleLiked = () => {
+    setLiked(!liked);
+    const currentLike = !liked ? 1 : -1;
+    setTotalLikes(totalLikes + currentLike);
+  };
+
   return (
     <div className="mx-auto m-2 sm:w-[550px]">
       <div className="card card-compact shadow-lg rounded-lg">
@@ -13,8 +23,18 @@ const PostCard = ({ post }) => {
         }
         <div className="card-body">
           <p>{postBody}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary capitalize">Details</button>
+          <div className="card-actions justify-between">
+            <button className="btn btn-ghost" onClick={handleLiked} title={liked ? 'Unlike' : 'Like'}>
+              {
+                liked ?
+                  <FaThumbsUp className="text-2xl"></FaThumbsUp>
+                  :
+                  <FaRegThumbsUp className="text-2xl"></FaRegThumbsUp>
+              }
+            </button>
+            <button className="btn btn-ghost">
+              <FaRegCommentAlt className="text-2xl"></FaRegCommentAlt>
+            </button>
           </div>
         </div>
       </div>
