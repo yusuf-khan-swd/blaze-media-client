7dfe2c5a70c6bf81bb16efce67c5f04bbec0416c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-10 13:09:31 +0600

    added Spinner and Loader

--- src/Page/Shared/Loading/Spinner.js
+++ src/Page/Shared/Loading/Spinner.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Loading = () => {
+const Spinner = () => {
   return (
-    <div className={`flex items-center justify-center space-x-2 h-screen`}>
+    <div className="flex items-center justify-center space-x-2">
       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
@@ -12,4 +12,4 @@ const Loading = () => {
   );
 };
 
-export default Loading;
+export default Spinner;
