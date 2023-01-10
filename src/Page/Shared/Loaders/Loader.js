70f9657f835a554181ab33c890448f397ed4762a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-10 13:10:04 +0600

    rename the folder and component name

--- src/Page/Shared/Loaders/Loader.js
+++ src/Page/Shared/Loaders/Loader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Loading = () => {
+const Loader = () => {
   return (
     <div className={`flex items-center justify-center space-x-2 h-screen`}>
       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
@@ -12,4 +12,4 @@ const Loading = () => {
   );
 };
 
-export default Loading;
+export default Loader;
